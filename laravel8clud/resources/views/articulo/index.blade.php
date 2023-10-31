43fa186e9d1d3bc6e6a2c04cc6e2c5dd023efda2
williambravohudson <almn.williambravo@cbtis66.edu.mx>
2023-10-30 22:10:42 -0600

    ya lo actualize yei

--- laravel8clud/resources/views/articulo/index.blade.php
+++ laravel8clud/resources/views/articulo/index.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.plantillabase');
 
-@section('contenido')
-<a href="articulos/create" class="btn btn-primary">Crear</a>
+@section('css')
+<link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel=stylesheet>
+@endsection
 
-<table class="table table-dark table-striped mt-4">
-    <thead>
+
+@section('contenido')
+<a href="articulos/create" class="btn btn-primary mb-3">Crear</a>
+
+<table id="articulos" class="table table-striped table-bordered shadow-lg mt-4" style="width:100%">
+    <thead class="bg-primary text-white">
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Codigo</th>
@@ -38,5 +43,19 @@
     </tbody>
 </table>
 
+@section('js')
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#articulos').DataTable({
+        "lengthMenu": [[5,10,50,-1],[5,10,50,"ALL"]]
+    });
+} );
+</script>
+
+@endsection
 
 @endsection
