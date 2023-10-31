e74e1fc8c62773875af955a1278cb1e7766d60a0
williambravohudson <almn.williambravo@cbtis66.edu.mx>
2023-10-30 23:23:49 -0600

    termine y medio miedo

--- laravel8clud/resources/views/articulo/index.blade.php
+++ laravel8clud/resources/views/articulo/index.blade.php
@@ -1,19 +1,20 @@
-@extends('layouts.plantillabase');
+@extends('adminlte::page')
 
-@section('css')
-<link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel=stylesheet>
-@endsection
+@section('title', 'CRUD con Laravel 8')
 
+@section('content_header')
+    <h1>Listado de artículos</h1>
+@stop
 
-@section('contenido')
-<a href="articulos/create" class="btn btn-primary mb-3">Crear</a>
+@section('content')
+   <a href="articulos/create" class="btn btn-primary mb-3">CREAR</a>
 
 <table id="articulos" class="table table-striped table-bordered shadow-lg mt-4" style="width:100%">
     <thead class="bg-primary text-white">
         <tr>
             <th scope="col">ID</th>
-            <th scope="col">Codigo</th>
-            <th scope="col">Descripcion</th>
+            <th scope="col">Código</th>
+            <th scope="col">Descripción</th>
             <th scope="col">Cantidad</th>
             <th scope="col">Precio</th>
             <th scope="col">Acciones</th>
@@ -22,40 +23,41 @@
     <tbody>
         @foreach ($articulos as $articulo)
         <tr>
-            <td>{{$articulo->id}}</td>
+            <td>{{ $articulo->id}}</td>
             <td>{{$articulo->codigo}}</td>
             <td>{{$articulo->descripcion}}</td>
             <td>{{$articulo->cantidad}}</td>
             <td>{{$articulo->precio}}</td>
             <td>
-
-             
-
-                <form action="{{ route ('articulos.destroy' ,$articulo->id)}}" method="POST">
-                <a href="/articulos/{{$articulo->id}}/edit" class="btn btn-info"> Editar</a>
+                <form action="{{ route ('articulos.destroy',$articulo->id)}}" method="POST">
+                <a href="/articulos/{{ $articulo->id}}/edit" class="btn btn-info">Editar</a>
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="btn btn-danger">borrar</button>
+                <button type="submit" class="btn btn-danger">Borrar</button>
                 </form>
             </td>
         </tr>
         @endforeach
     </tbody>
 </table>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@stop
 
 @section('js')
-<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap5.min.js"></script>
 
 <script>
 $(document).ready(function() {
     $('#articulos').DataTable({
-        "lengthMenu": [[5,10,50,-1],[5,10,50,"ALL"]]
+        "lengthMenu": [[5,10, 50, -1], [5, 10, 50, "All"]]
     });
 } );
 </script>
 
-@endsection
-
-@endsection
+@stop
